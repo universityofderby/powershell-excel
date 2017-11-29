769393e72036caf05b3e3f73269555eec4ffc9e3
richardlock <richardlock@users.noreply.github.com>
2017-11-29 08:44:17 +0000

    Added default value for Match parameter

--- Get-LinkedExcelFiles.psm1
+++ Get-LinkedExcelFiles.psm1
@@ -27,23 +27,23 @@ Specify the depth of recursion.
 Object
 
 .EXAMPLE
-PS> Get-LinkedExcelFiles -Path "C:\Temp" -Match ".*\[.*\].*"
+PS> Get-LinkedExcelFiles -Path "C:\Temp"
 Get Excel files containing external links recursively under path "C:\Temp".
 
 .EXAMPLE
-PS> Get-LinkedExcelFiles -Path "C:\Temp" -Match ".*\[.*\].*" | Export-Csv -Path C:\Temp\LinkedExcelFiles.csv -NoTypeInformation
+PS> Get-LinkedExcelFiles -Path "C:\Temp" | Export-Csv -Path C:\Temp\LinkedExcelFiles.csv -NoTypeInformation
 Get Excel files containing external links recursively under path "C:\Temp" and export output to CSV file.
 
 .EXAMPLE
-PS> Get-LinkedExcelFiles -Path "C:\Temp" -Match ".*\[.*\].*" -WhatIf
+PS> Get-LinkedExcelFiles -Path "C:\Temp" -WhatIf
 Get Excel files recursively under path "C:\Temp" but do not open files.
 
 .EXAMPLE
-PS> Get-LinkedExcelFiles -Path "C:\Temp" -Match ".*\[.*\].*" -Depth 2
+PS> Get-LinkedExcelFiles -Path "C:\Temp" -Depth 2
 Get Excel files containing external links to a depth of two subdirectories under path "C:\Temp".
 
 .EXAMPLE
-PS> Get-LinkedExcelFiles -Path "C:\Temp" -Match ".*\[.*\].*" -Recurse:$false
+PS> Get-LinkedExcelFiles -Path "C:\Temp" -Recurse:$false
 Get Excel files containing external links non-recursively under path "C:\Temp".
 
 .EXAMPLE
@@ -55,6 +55,9 @@ PS> Get-LinkedExcelFiles -Path "S:\Department\Shared" -Match ".*\\\\fileserver1.
 Get Excel files containing links to "\\fileserver1" or "\\fileserver2" under path "S:\Department\Shared" using filter "*.xlsx".
 
 .NOTES
+  Version: 0.2.1 - Added default value for Match parameter
+  Date: 2017-11-29
+  
   Version: 0.2.0 - Added -WhatIf functionality
   Date: 2017-11-27
 
@@ -68,7 +71,7 @@ Get Excel files containing links to "\\fileserver1" or "\\fileserver2" under pat
 
   Param (
     [Parameter(Mandatory = $true, ValueFromPipeline = $true)] [String]$Path,
-    [Parameter(Mandatory = $true)] [String]$Match,
+    [Parameter()] [String]$Match = ".*\[.*\].*",
     [Parameter()] [String]$Filter = "*.xls",
     [Parameter()] [Boolean]$Recurse = $true,
     [Parameter(ParameterSetName = "Depth")] [Int32]$Depth
